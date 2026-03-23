be43bf4d50a0732f5cf249bf49d737e8ecb5652e
caleb <calebh101dev@icloud.com>
2026-03-23 12:06:38 -0500

    Add command for toggling timestamps

--- index.tsx
+++ index.tsx
@@ -4,10 +4,14 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+import { ApplicationCommandInputType } from "@api/Commands";
+import { showNotification } from "@api/Notifications";
 import { definePluginSettings } from "@api/Settings";
 import { Devs } from "@utils/constants";
 import { Logger } from "@utils/Logger";
 import definePlugin, { OptionType } from "@utils/types";
+import { Command } from "@vencord/discord-types";
+import { FluxDispatcher } from "@webpack/common";
 
 const enabled: boolean = true;
 const debug: boolean = false;
@@ -26,11 +30,33 @@ const settings = definePluginSettings({
     },
 });
 
+const commands: Command[] = [
+    {
+        name: "messagetimestamps",
+        description: "Enable/disable MessageCorrector's debug message timestamps.",
+        inputType: ApplicationCommandInputType.BUILT_IN,
+        execute(args, context) {
+            settings.store.showTimestamps = !settings.store.showTimestamps;
+            FluxDispatcher.dispatch({ type: "CHANNEL_SELECT" });
+
+            showNotification({
+                title: "MessageCorrector",
+                body: `Timestamps are now ${settings.store.showTimestamps ? "enabled" : "disabled"}. The channel will be reloaded.`,
+            });
+
+            setTimeout(() => {
+                FluxDispatcher.dispatch({ type: "CHANNEL_SELECT", channelId: context.channel.id, guildId: context.guild?.id ?? null });
+            }, 500);
+        },
+    },
+];
+
 export default definePlugin({
     name: "MessageCorrector",
     description: "Corrects the order of messages in the chat, based on when the message reached Discord's servers.",
     authors: [Devs.Calebh101],
     settings: settings,
+    commands: commands,
 
     start() {
         if (enabled) logger.log("Started in " + (debug ? "debug" : "standard") + " mode");
