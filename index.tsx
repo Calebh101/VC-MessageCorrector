11371605787502633fdcb8226fb5b9a64435f805
caleb <calebh101dev@icloud.com>
2026-04-04 22:56:06 -0500

    Documentation, better descriptions, and fix channel reloading bug

--- index.tsx
+++ index.tsx
@@ -24,7 +24,7 @@ const settings = definePluginSettings({
         default: true,
     },
     showTimestamps: {
-        description: "Show UTC ISO 8601 timestamps on messages. This is mainly used for debugging.",
+        description: "Show UTC ISO 8601 timestamps on messages. This is mainly used for debugging. This setting can also be toggled by /messagetimestamps.",
         type: OptionType.BOOLEAN,
         default: false,
     },
@@ -33,11 +33,17 @@ const settings = definePluginSettings({
 const commands: Command[] = [
     {
         name: "messagetimestamps",
-        description: "Enable/disable MessageCorrector's debug message timestamps.",
+        description: "Enable/disable MessageCorrector's message timestamps debug feature.",
         inputType: ApplicationCommandInputType.BUILT_IN,
         execute(args, context) {
+            // We manually toggle the setting, then deselect all channels,
+            // because I couldn't find a way to just reload a channel. Thanks Discord.
+            // We then show a notification, wait half a second, and select the channel we were in.
+            // I tested this for both guilds and DMs, and they both work.
+            // This is a kinda hacky solution, but I was really only meaning this as a debugging feature anyways.
+
             settings.store.showTimestamps = !settings.store.showTimestamps;
-            FluxDispatcher.dispatch({ type: "CHANNEL_SELECT" });
+            FluxDispatcher.dispatch({ type: "CHANNEL_SELECT", channelId: null, guildId: null });
 
             showNotification({
                 title: "MessageCorrector",
@@ -67,6 +73,7 @@ export default definePlugin({
     },
 
     patches: [
+        // When Discord's returning the messages from somewhere, we wrap it in our custom function.
         {
             find: ",showNewMessagesBar:!",
             replacement: {
@@ -77,6 +84,7 @@ export default definePlugin({
         },
     ],
 
+    // The actual message timestamps thing
     renderMessageAccessory: props => {
         if (!settings.store.showTimestamps) return null;
         if (!props.message?.timestamp) return null;
@@ -125,7 +133,8 @@ export default definePlugin({
 
             if (settings.store.allowReordering) {
                 // Sort everything by their timestamp.
-                // The timestamp is when the message reached Discord's servers.
+                // The timestamp is when the message reached Discord's servers,
+                // **not** the client's stores.
 
                 messages._array = [...messages._array].sort((a, b) => {
                     const tA = new Date(a.timestamp).getTime();
